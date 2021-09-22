7175ad28d1ce5c3e02d6e398cda8d7ebc4cd4d40
chawy1989 <wilsito.celada@gmail.com>
2021-09-22 16:29:38 -0600

    Primeros pasos en POO

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace Porgramacion_Orientada_a_Objetos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Persona persona = new Persona();
+            persona.nombre = "Wilson";
+            persona.edad = 32;
+            persona.genero = 'M';
+            persona.fechaNacimiento = new DateTime(1989, 2, 1);
+            Console.WriteLine($"Nombre: {persona.nombre}");
+            Console.WriteLine($"Edad: {persona.edad}");
+            Console.WriteLine($"Genero: {persona.genero}");
+            Console.WriteLine($"Fecha Nacimiento : {persona.fechaNacimiento}");
         }
+    }
+
+    class Persona
+    {
+        public string nombre;
+        public int edad;
+        public char genero;
+        public DateTime fechaNacimiento;
     }
 }
