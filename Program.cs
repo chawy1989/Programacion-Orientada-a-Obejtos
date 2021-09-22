265a7005c9da1cab3fdfd5506eab1911525c7daa
chawy1989 <wilsito.celada@gmail.com>
2021-09-22 15:54:10 -0600

    Inicio del programa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Porgramacion_Orientada_a_Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
