d99d3532bc37ad60c6bf55b9eae5a39be642f90d
chawy1989 <wilsito.celada@gmail.com>
2021-09-22 16:59:14 -0600

    Se agregaron propiedades Get and Set

--- Program.cs
+++ Program.cs
@@ -7,22 +7,46 @@ namespace Porgramacion_Orientada_a_Objetos
         static void Main(string[] args)
         {
             Persona persona = new Persona();
-            persona.nombre = "Wilson";
-            persona.edad = 32;
-            persona.genero = 'M';
-            persona.fechaNacimiento = new DateTime(1989, 2, 1);
-            Console.WriteLine($"Nombre: {persona.nombre}");
-            Console.WriteLine($"Edad: {persona.edad}");
-            Console.WriteLine($"Genero: {persona.genero}");
-            Console.WriteLine($"Fecha Nacimiento : {persona.fechaNacimiento}");
+            persona.Nombre = "Wilson";
+            persona.Edad = 32;
+            persona.Genero = 'M';
+            persona.FechaNacimiento = new DateTime(1989, 2, 1);
+            persona.Dpi = "2240619800101";
+            Console.WriteLine($"Nombre: {persona.Nombre}");
+            Console.WriteLine($"Edad: {persona.Edad}");
+            Console.WriteLine($"Genero: {persona.Genero}");
+            Console.WriteLine($"Fecha Nacimiento : {persona.FechaNacimiento}");
+            Console.WriteLine($"DPI: {persona.Dpi}");
+            
         }
     }
 
     class Persona
     {
-        public string nombre;
-        public int edad;
-        public char genero;
-        public DateTime fechaNacimiento;
+        string nombre;
+        int edad;
+        char genero;
+        DateTime fechaNacimiento;
+        string dpi;
+
+        //Defincion de Propiedades
+        //Propiedad que tiene logica incluida
+        public int Edad
+        {
+            get { return edad; }
+            set
+            {
+                if (value < 0 || value > 100)
+                    Console.WriteLine("Edad Invalida");
+                else
+                    edad = value;
+            }
+        }
+        //Instruccion de propiedades automaticas
+        public string Nombre { get; set; }
+        public char Genero { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Dpi { get; set; }
+        
     }
 }
