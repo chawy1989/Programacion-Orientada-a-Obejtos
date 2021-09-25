d1a42f427f118f433477d34698027dd24fc602cc
chawy1989 <wilsito.celada@gmail.com>
2021-09-24 23:06:50 -0600

    Agregue una clase nueva pero no me dejo usarla

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Porgramacion_Orientada_a_Objetos
+{
+    class Class1
+    {
+        
+        
+        
+       
+    }
+}
---
+++ Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Porgramacion_Orientada_a_Objetos
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Wilson Celada";
+            persona.Edad = 32;
+            persona.Genero = 'M';
+            persona.FechaNacimiento = new DateTime(1989, 2, 1);
+            persona.Dpi = "2240629800101";
+            Console.WriteLine($"Nombre: {persona.Nombre}");
+            Console.WriteLine($"Genero {persona.Genero}"); 
+            Console.WriteLine($" Nacimiento: {persona.FechaNacimiento}");
+            Console.WriteLine($"Edad: {persona.Edad}");
+            Console.WriteLine($"DPI: {persona.Dpi}");
+        }
+        
+    class Persona
+        {
+            string nombre;
+            int edad;
+            char genero;
+            DateTime fechaNacimiento;
+            string dpi;
+            public int Edad { get => edad; set => edad = value; }
+            public string Nombre { get => nombre; set => nombre = value; }
+            public char Genero { get => genero; set => genero = value; }
+            public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+            public string Dpi { get => dpi; set => dpi = value; }
+
+        }
+
+        
+
+    }
+}
